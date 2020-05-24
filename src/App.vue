0e15fd7549a7b1639a8aeef943d3c7bee40a40d8
kamoshibito510 <kamoshibito510@icloud.com>
2020-05-24 22:51:44 +0900

    音楽検索アプリ

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <template>
-  <div></div>
+  <div class="root">
+    <Search class="search" @loadStart="onLoadStart" @loadComplete="onLoadComplete"/>
+    <Result :items="items" :loadProgress="loadProgress"/>
+  </div>
 </template>
 
 <script>
-
+import Search from "@/components/Search";
+import Result from "@/components/Result";
+ 
+ 
+export default {
+  data() {
+    return {
+      items: [],
+      loadProgress: false,
+    };
+  },
+  methods: {
+    onLoadStart() {
+      this.loadProgress = true;
+    },
+    onLoadComplete({ results }) {
+      this.items = results;
+      this.loadProgress = false;
+    },
+  },
+  components: {
+    Search,
+    Result,
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.root {
+  padding-top: 70px;
+}
+ 
+ 
+.search {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
 </style>
+ 
